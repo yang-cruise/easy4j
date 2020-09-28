76c9773a089a6e06beb1fe5fd9d75e998b6c6427
yangzongmin <zongmin.yang@sohan.hk>
2020-09-28 15:54:32 +0800

    添加后端示例

---
+++ easy4j-sample/src/main/java/org/example/SampleMainType.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangzongmin
+ * @since 2020/9/27
+ */
+@SpringBootApplication
+public class SampleMainType {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMainType.class, args);
+    }
+
+}
